f453920372c7dacaa901ea8365807615b11be3a1
Muhammed ebrahim <ebrahimmuhammed479@gmail.com>
2024-04-20 14:37:33 +0300

    shadow extension function implementation

--- app/src/main/java/com/example/androidcalculator/MainActivity.kt
+++ app/src/main/java/com/example/androidcalculator/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.androidcalculator
 
+import android.graphics.BlurMaskFilter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -7,6 +8,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -14,8 +16,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.androidcalculator.ui.theme.AndroidCalculatorTheme
@@ -83,11 +90,38 @@ fun CalcButton(
         }
     }
 }
+// shadow extension function to modifier
+fun Modifier.shadow(
+    color: Color = Color.Black,
+    offsetX: Dp =0.dp,
+    offsetY: Dp = 0.dp,
+    blurRadius: Dp = 0.dp,
+) = then(
+    drawBehind {
+        val paint = Paint()
+        val frameworkPaint = paint.asFrameworkPaint()
+        if (blurRadius != 0.dp) {
+            frameworkPaint.maskFilter = (BlurMaskFilter(blurRadius.toPx(), BlurMaskFilter.Blur.NORMAL))
+        }
+        frameworkPaint.color = color.toArgb()
+        drawIntoCanvas {canvas ->
+            val leftP = offsetX.toPx()
+            val topP = offsetY.toPx()
+            val rightP = size.width + topP
+            val bottomP = size.height + leftP
+            canvas.drawRect(
+                left =  leftP, right =  rightP, top  =  topP,
+                bottom =  bottomP, paint = paint
+            )
+        }
+    }
+)
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AndroidCalculatorTheme {
-
+        CalcButton(text = "1", modifier = Modifier.size(100.dp)) {
+        }
     }
 }
